aa61008d57dbf16bf34fe19d23bcfc7ae15c5b05
locnguyenhuu0024 <18050078@student.bdu.edu.vn>
2021-07-21 10:33:41 +0700

    updated at 10:33, 21/07/2021

--- index.js
+++ index.js
@@ -25,14 +25,18 @@ app.get('/news', (req, res) => {
     res.sendFile(path.join(__dirname+'/views/news.html'))
 })
 
-app.get('/detail-news', (req, res) => {
-    res.sendFile(path.join(__dirname+'/views/detail-news.html'))
+app.get('/detailnews', (req, res) => {
+    res.sendFile(path.join(__dirname+'/views/detailnews.html'))
 })
 
 app.get('/calendars', (req, res) => {
     res.sendFile(path.join(__dirname+'/views/calendarcourse.html'))
 })
 
-app.get('/detail-course', (req, res) => {
-    res.sendFile(path.join(__dirname+'/views/detail-course.html'))
+app.get('/listcourse', (req, res) => {
+    res.sendFile(path.join(__dirname+'/views/listcourses.html'))
+})
+
+app.get('/detailcourse', (req, res) => {
+    res.sendFile(path.join(__dirname+'/views/detailcourse.html'))
 })
