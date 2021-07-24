b4ec07fd7e69f5ac71d97f6ecd47e16b2746da76
locnguyenhuu0024 <18050078@student.bdu.edu.vn>
2021-07-24 19:26:24 +0700

    updated something at 19:26, 24/07/2021

--- public/js/notify.js
+++ public/js/notify.js
@@ -10,49 +10,82 @@ const statusNotify = {
     failColor: 'background-color: rgba(255, 82, 82, 1); color: white;',
     failContent: 'Vui lòng nhập đầy đủ thông tin.',
     failInputColor:  'background-color: rgba(255, 82, 82, .3);',
+    failName: 'Hãy đảm bảo bạn nhập đúng họ tên, và họ tên phải hơn 3 ký tự!',
+    failPhone: 'Hãy đảm bảo số điện thoại được nhập đủ 10 - 11 số!',
+    failEmail: 'Email sai định dạng, vui lòng nhập lại!'
 };
 
-btnRegister.addEventListener('click', (e) => {
+function checkNull(){
     let tam = 0;
-    
-    function checkNull(){
-        inputRegisters.forEach((inputRegister) => {
-            if(!inputRegister.value && inputRegister.hasAttribute('required')){
-                tam = tam + 1;
-                inputRegister.setAttribute('style', statusNotify.failInputColor);
-            }else{
-                inputRegister.setAttribute('style', 'background-color: $white;');
-                inputRegister.setAttribute('readonly', 'true');
-            }
 
-            if(!inputRegister.hasAttribute('required')){
-                inputRegister.removeAttribute('readonly');
-            }
-        });
-
-        if(tam == 0){
-            setTimeout(() => {
-                notify.classList.remove('notify-float-hide');
-                notify.classList.add('notify-float-show');
-                btnRegister.setAttribute('style', 'visibility: hidden;');
-                notify.setAttribute('style', statusNotify.passColor);
-                notifyContent.innerHTML = statusNotify.passContent;
-            }, 1000);
+    inputRegisters.forEach((inputRegister) => {
+        if(!inputRegister.value && inputRegister.hasAttribute('required')){
+            tam = tam + 1;
+            inputRegister.setAttribute('style', statusNotify.failInputColor);
         }else{
-            setTimeout(() => {
-                notify.classList.add('notify-float-show');
-                notify.setAttribute('style', statusNotify.failColor);
-                notifyContent.innerHTML = statusNotify.failContent;
-            }, 1000);
+            inputRegister.setAttribute('style', 'background-color: $white;');
+            inputRegister.setAttribute('readonly', 'true');
         }
 
-        setTimeout(() => {
-            notify.classList.add('notify-float-hide');
-            notify.classList.remove('notify-float-show');
-        }, 5600);
-    }   
-    
-    checkNull();
+        if(!inputRegister.hasAttribute('required')){
+            inputRegister.removeAttribute('readonly');
+        }
+    });
+
+    if(tam != 0) return false;
+    return true;
+}  
+
+const inputName = document.getElementById('stuName');
+const inputPhone = document.getElementById('phone');
+const inputEmail = document.getElementById('email');
+
+function checkName(){
+    let modalName = /^[a-zA-Z ]+$/;
+
+    if(inputName.value.length < 3 || !modalName.test(inputName.value)) return false;
+    return true;
+}
+
+function checkPhone(){
+    const modalPhone = /^[0-9]+$/;
+
+    if(modalPhone.test(inputPhone.value))
+        if(inputPhone.value.length >= 10 && inputPhone.value.length <= 11)
+            return true;
+        return false;
+}
+ 
+function checkEmail(){
+    const modalEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if(!modalEmail.test(inputEmail.value)) return false;
+    return true;
+}
+
+btnRegister.addEventListener('click', (e) => {
+    console.log(checkEmail());
+
+    // if(!checkNull()){
+    //     setTimeout(() => {
+    //         notify.classList.add('notify-float-show');notify.classList.remove('notify-float-hide');
+    //         notify.setAttribute('style', statusNotify.failColor);
+    //         notifyContent.innerHTML = statusNotify.failContent;
+    //     }, 600);
+    // }else{
+    //     setTimeout(() => {
+    //         notify.classList.remove('notify-float-hide');
+    //         notify.classList.add('notify-float-show');
+    //         btnRegister.setAttribute('style', 'visibility: hidden;');
+    //         notify.setAttribute('style', statusNotify.passColor);
+    //         notifyContent.innerHTML = statusNotify.passContent;
+    //     }, 600);
+    // }
+
+    setTimeout(() => {
+        notify.classList.add('notify-float-hide');
+        notify.classList.remove('notify-float-show');
+    }, 4600);
 });    
 
 btnCloseNotify.addEventListener('click', (e) => {
